afb442311b3fe59acec3177dba8dacbf1f7f12c1
ryaasabsar@gmail.com <ryaasabsar@gmail.com>
2019-12-07 04:10:24 +0700

    Init LCD and LCD Testing

---
+++ init_lcd.asm
@@ -0,0 +1,42 @@
+INIT_LCD_MAIN:
+	rcall INIT_LCD
+
+	ser temp
+	out DDRA,temp ; Set port A as output
+	out DDRB,temp ; Set port B as output
+
+	rcall WRITE_TEXT
+
+END_LCD:
+	ret
+
+INIT_LCD:
+	cbi PORTA,1 ; CLR RS
+	ldi temp,0x38 ; MOV DATA,0x38 --> 8bit, 2line, 5x7
+	out PORTB,temp
+	sbi PORTA,0 ; SETB EN
+	cbi PORTA,0 ; CLR EN
+	rcall DELAY_01
+	cbi PORTA,1 ; CLR RS
+	ldi temp,$0E ; MOV DATA,0x0E --> disp ON, cursor ON, blink OFF
+	out PORTB,temp
+	sbi PORTA,0 ; SETB EN
+	cbi PORTA,0 ; CLR EN
+	rcall DELAY_01
+	rcall CLEAR_LCD ; CLEAR LCD
+	cbi PORTA,1 ; CLR RS
+	ldi temp,$06 ; MOV DATA,0x06 --> increase cursor, display sroll OFF
+	out PORTB,temp
+	sbi PORTA,0 ; SETB EN
+	cbi PORTA,0 ; CLR EN
+	rcall DELAY_01
+	ret
+
+CLEAR_LCD:
+	cbi PORTA,1 ; CLR RS
+	ldi temp,$01 ; MOV DATA,0x01
+	out PORTB,temp
+	sbi PORTA,0 ; SETB EN
+	cbi PORTA,0 ; CLR EN
+	rcall DELAY_01
+	ret
